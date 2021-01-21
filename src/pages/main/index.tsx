4d622a43647ecc6dce2a5f4165e76ba2054a03ae
marianna <vergunmari@gmail.com>
2021-01-21 15:02:11 +0300

    feat: add movies list

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { StoreMovies } from "../../store/movies";
+import React, { ReactElement } from "react";
+import { NMovies, StoreMovies } from "../../store/movies";
+import { Link } from "react-router-dom";
 import { MOVIE_API_KEY } from "../../consts";
+import { List, Avatar } from "antd";
 
-export function SearchPage(): React.ReactElement {
+export function MoviesPage(): React.ReactElement {
   useFetcherCommonData();
 
-  const moviesData: any = StoreMovies.useSelector((store: any) => store);
-  console.log("@@ storeData", moviesData);
+  const moviesData: any = StoreMovies.useSelector(
+    (store: NMovies.IStore) => store
+  );
   return (
     <>
       <div>
-        <h1>Search page</h1>
+        <h1>Movies page</h1>
+        <MoviesList moviesData={moviesData} />
         {moviesData.loading ? (
           "Loading..."
         ) : (
@@ -28,6 +32,7 @@ function useFetcherCommonData() {
 
   React.useEffect(function fetchData() {
     fetchMoviesData();
+    console.log("store", StoreMovies);
 
     async function fetchMoviesData() {
       try {
@@ -51,4 +56,52 @@ function useFetcherCommonData() {
   }, []);
 
   return null;
+}
+
+function MoviesList(props: { moviesData: NMovies.IStore }): ReactElement {
+  const { moviesData } = props;
+  console.log("MD", moviesData.list);
+
+  return (
+    <List
+      itemLayout="vertical"
+      size="small"
+      pagination={{
+        onChange: (page) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      }}
+      dataSource={moviesData.list}
+      footer={
+        <div>
+          <b>Movie discovery</b>
+        </div>
+      }
+      renderItem={(movieId) => {
+        const movie = moviesData.map[movieId];
+        return (
+          <Link to={`/card/${movieId}`}>
+            <List.Item
+              key={movieId}
+              extra={
+                <img
+                  width={120}
+                  height={200}
+                  alt="movie poster"
+                  src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`}
+                />
+              }
+            >
+              <List.Item.Meta title={<a>{movie.title}</a>} />
+              {`Popularity: ${movie.popularity}`}
+              {`Date of release: ${new Date(
+                movie.release_date
+              ).toDateString()}`}
+            </List.Item>
+          </Link>
+        );
+      }}
+    />
+  );
 }
